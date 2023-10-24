7b8e3f136e33a6301138ac7329ec89f1920cff8d
TestUser1238g <121501621+TestUser1238g@users.noreply.github.com>
2023-10-24 17:09:13 +0530

    Update AppiumTest.java

--- src/test/java/TestAutomation/TestingFramework/AppiumTest.java
+++ src/test/java/TestAutomation/TestingFramework/AppiumTest.java
@@ -6,8 +6,8 @@ public class AppiumTest {
 	
 	@Test
 	public void getMethodclass() {
+		System.out.println("Appium Test Automation Changes");
 		System.out.println("Appium Test");
-		System.out.println("Appium Test1 is updated");
 	}
 	
 	@Test(groups= {"Regression"})
